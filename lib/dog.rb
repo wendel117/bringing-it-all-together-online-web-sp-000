736f1dbb280995332968a66eea38e2ccdf46f34a
Wendy Hamilton <wdhamilton117@gmail.com>
2019-11-04 17:11:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -82,4 +82,11 @@ end
     end
     dog
   end
+  def update
+  sql = <<-SQL
+    UPDATE dogs SET name = ?, breed = ?
+    WHERE id = ?
+  SQL
+  DB[:conn].execute(sql, self.name, self.breed, self.id)
+end
 end
